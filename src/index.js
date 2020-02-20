59cf1376f08cd072f0b92a4b872d2fea3178b273
Andrei Kozik <13pgs1.kozik.a@pdu.by>
2020-02-20 09:16:02 +0300

    complite tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0;
+    array.sort((a, b) => a - b);
+    return array[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) return 0;
+    array.sort((a, b) => b - a);
+    return array[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0;
+    let result = array.reduce((a, b) => a + b) / array.length;
+    return result;
+};
